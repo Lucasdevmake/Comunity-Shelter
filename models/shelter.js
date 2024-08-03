00dac3fb3f4d15487d42ec877d4f74d010a502ac
Lucasdevmake <lucasanc14@gmail.com>
2024-08-02 22:41:36 -0300

    master: validações create shelter e recursos

--- models/shelter.js
+++ models/shelter.js
@@ -1,4 +1,28 @@
+const { ResourcePatternTypes } = require("kafkajs");
 const mongoose = require("mongoose");
+
+const ResourceSchema = new mongoose.Schema({
+    doctor: {
+        type: Number,
+        required: true,
+    },
+    volunteer: {
+        type: Number,
+        required: true,
+    },
+    supplyKit: {
+        type: Number,
+        required: true,
+    },
+    basicBasket: {
+        type: Number,
+        required: true,
+    },
+    transport: {
+        type: Number,
+        required: true,
+    }
+});
 
 const ShelterSchema = new mongoose.Schema({
     _id: String,
@@ -9,6 +33,10 @@ const ShelterSchema = new mongoose.Schema({
     occupancy: {
         type: Number,
         default: 0
+    },
+    resource: {
+        type: ResourceSchema,
+        required: true
     }
 });
 
--- services/createShelter.js
+++ services/createShelter.js
@@ -1,15 +1,26 @@
 const Shelter = require("../models/shelter");
 const connect = require("../database");
 const uuid = require("uuid");
+const yup = require("yup");
 
 module.exports.createShelter = async(event) => {
     const body = JSON.parse(event.body);
+    const valid = await validadeBody(body);
+
+    if (!valid) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({message: "Invalid Request"})
+        }
+    }
+
     const shelter = {
         _id: uuid.v4(),
         name: body.name,
         email: body.email,
         streetAdress: body.streetAdress,
-        capacity: body.capacity
+        capacity: body.capacity,
+        resource: body.resource
     };
 
     await connect();
@@ -19,5 +30,31 @@ module.exports.createShelter = async(event) => {
     return {
         statusCode: 201, //created
         body: JSON.stringify(shelter)
+    }
+};
+
+async function validadeBody(body) {
+    try {
+        const schema = yup.object().shape({
+            name: yup.string().required(),
+            streetAdress: yup.string().required(),
+            capacity: yup.number().required().positive().integer(),
+            email: yup.string().email().required(),
+            occupancy: yup.number().required().positive().integer(),
+            resource: yup.object().shape({
+                doctor: yup.number().required().positive().integer(),
+                volunteer: yup.number().required().positive().integer(),
+                supplyKit: yup.number().required().positive().integer(),
+                basicBasket: yup.number().required().positive().integer(),
+                transport: yup.number().required().positive().integer()
+            })
+        })
+
+        await schema.validate(body);
+        return true;
+
+    } catch(error){
+        console.log(error);
+        return false;
     }
 };
